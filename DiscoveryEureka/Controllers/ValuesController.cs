6ffd2a5f283d41cc4b671c6484ba92023bb52fa9
mohsen.mhbi <mohsen.mhbi@gmail.com>
2021-03-23 13:49:39 +0430

    Add project files.

---
+++ DiscoveryEureka/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Common.Discovery;
+using Steeltoe.Discovery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DiscoveryEureka.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        DiscoveryHttpClientHandler _handler;
+        public ValuesController(ILogger<ValuesController> logger, IDiscoveryClient client)
+        {
+            _logger = logger;
+            _handler = new DiscoveryHttpClientHandler(client);
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var client = new HttpClient(_handler, false);
+            return await client.GetStringAsync("http://EurekaRegistery/api/Values");
+        }
+    }
+}
